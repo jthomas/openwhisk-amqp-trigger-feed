1401cfdfac46ae1f09749f78d546fc48c19aeaf6
James Thomas <james@jamesthom.as>
2019-05-20 16:48:44 +0100

    Adding error handling on connection issues.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const AMQP = require('./lib/amqp.js')
 const QueueProcessor = require('./lib/queue_processor.js')
 const Validate = require('./lib/validate.js')
 const amqp = require('amqplib')
 
-// - Need to add logging
-// - Need to handle errors gracefully
+const open_channel = async (url, options) => {
+  const open = await amqp.connect(url, options)
+  const channel = await open.createChannel()
+  return channel
+}
+
+const check_queue = async (url, options, queue) => {
+  const channel = await open_channel(url, options)
+  await channel.checkQueue(queue)
+}
+
 module.exports = function (triggerManager, logger) {
   const triggers = new Map()
 
-  // add integration tests for this!
   const format_message = (message, format = 'utf-8') => {
     if (format === 'json') {
       return JSON.parse(message.toString('utf-8'))
@@ -18,6 +25,7 @@ module.exports = function (triggerManager, logger) {
   }
 
   const add = async (id, details) => {
+    logger.debug(`amqp-trigger-feed`, 'add() called', id, details)
     // if the trigger is being updated, reset system state for trigger queue.
     if (triggers.has(id)) {
       remove(id)
@@ -25,21 +33,39 @@ module.exports = function (triggerManager, logger) {
 
     const { url, queue, format, cert } = details
 
-    const channel = await AMQP(amqp, url, { ca: [cert] }, queue)
-    const onmessage = msg => { 
-      // need to handle null
-      return triggerManager.fireTrigger(id, { msg: format_message(msg.content, format) })
-    }
-    const queue_processor = await QueueProcessor(channel, queue, onmessage)
+    const channel = await open_channel(url, { ca: [cert] } )
 
-    triggers.set(id, channel)
+    logger.info(`amqp-trigger-feed`, `opened new connection channel at (${url}) for trigger: ${id}`)
+
+    channel.on('error', (err) => {
+      logger.error('amqp-trigger-feed', `queue channel error (${url}) for trigger ${id}`, err)
+      triggerManager.disableTrigger(id, null, err.message)
+    })
+
+    const onmessage = msg => { 
+      const evt = { msg: format_message(msg.content, format) }
+      logger.debug(`amqp-trigger-feed`, `firing trigger (${id}) with event:`, evt)
+      return triggerManager.fireTrigger(id, evt)
+    }
+
+    const queue_processor = await QueueProcessor(channel, queue, onmessage, logger, id)
+    logger.info(`amqp-trigger-feed`, `queue processor (${queue}) started for trigger: ${id}`)
+
+    queue_processor.on('error', err => {
+      logger.error('amqp-trigger-feed', `error from processing queue messages for trigger ${id}`, err)
+      triggerManager.disableTrigger(id, null, err.message)
+    })
+
+    triggers.set(id, queue_processor)
   }
   
   const remove = async id => {
+    logger.debug(`amqp-trigger-feed`, 'remove() called', id)
     if (!triggers.has(id)) return
 
-    const channel = triggers.get(id)
-    await channel.connection.close()
+    const queue_processor = triggers.get(id)
+    await queue_processor.stop()
+    await queue_processor.channel.connection.close()
 
     triggers.delete(id)
   }
@@ -47,4 +73,4 @@ module.exports = function (triggerManager, logger) {
   return { add, remove }
 }
 
-module.exports.validate = async params => Validate(params, (url, options, queue) => AMQP(amqp, url, options, queue))
+module.exports.validate = async params => Validate(params, check_queue)
--- test/unit/queue_processor.js
+++ test/unit/queue_processor.js
@@ -1,15 +1,37 @@
 import test from 'ava'
 import QueueProcessor from '../../lib/queue_processor.js'
 
+const logger = { error: () => {}, debug: () => {}, info: () => {} }
+
 test('should do set channel pre-fetch value to 1', async t => {
   t.plan(1)
 
   const queue = 'some-queue-name'
   const channel = {
     prefetch: num => t.is(num, 1),
+    checkQueue: async () => {},
     consume: async () => {}
   }
-  await QueueProcessor(channel, queue)
+  await QueueProcessor(channel, queue, null, logger)
+})
+
+test('should check queue exists before consuming messages', async t => {
+  t.plan(2)
+
+  const queue = 'some-queue-name'
+  let checked_queue_exists = false
+
+  const channel = {
+    checkQueue: (_queue) => {
+      t.is(_queue, queue)
+      checked_queue_exists = true
+    },
+    prefetch: () => {},
+    consume: async () => {
+      t.true(checked_queue_exists)
+    }
+  }
+  await QueueProcessor(channel, queue, null, logger)
 })
 
 test('should fire callback for each message & ack after completion', async t => {
@@ -17,7 +39,7 @@ test('should fire callback for each message & ack after completion', async t => 
 
   let ack_fired = false
   let callback = null
-  const msg = Buffer.from('hello world')
+  const msg = { fields: {}, content: Buffer.from('hello world'), properties: {} }
   const onmessage = async (_msg) => { 
     t.is(_msg, msg)
     t.false(ack_fired)
@@ -26,6 +48,7 @@ test('should fire callback for each message & ack after completion', async t => 
   const queue = 'some-queue-name'
   const channel = {
     prefetch: () => {},
+    checkQueue: async () => {},
     consume: async (_queue, cb) => {
       t.is(_queue, queue)
       callback = cb
@@ -35,7 +58,7 @@ test('should fire callback for each message & ack after completion', async t => 
       ack_fired = true
     }
   }
-  await QueueProcessor(channel, queue, onmessage)
+  await QueueProcessor(channel, queue, onmessage, logger)
   await callback(msg)
 
   t.true(ack_fired) 
@@ -44,35 +67,84 @@ test('should fire callback for each message & ack after completion', async t => 
 test('should stop listening to messages on cancellation', async t => {
   t.plan(1)
 
-  const consumer_id = 'some-uuid'
+  const consumerTag = 'some-uuid'
   const queue = 'some-queue-name'
 
   const onmessage = () => t.fail()
   const channel = {
     prefetch: () => {},
-    consume: async () => consumer_id,
-    cancel: async (id) => t.is(id, consumer_id)
+    checkQueue: async () => {},
+    consume: async () => ({ consumerTag }),
+    cancel: async (id) => t.is(id, consumerTag )
   }
 
-  const stop = await QueueProcessor(channel, queue, onmessage)
-  await stop()
+  const qp = await QueueProcessor(channel, queue, onmessage, logger)
+  await qp.stop()
 })
 
-test('should not fire ack when callback throw errors', async t => {
+test('should not fire ack and emit error event when onmessage fails', async t => {
   t.plan(1)
 
-  let ack_fired = false
   let callback = null
-  const msg = Buffer.from('hello world')
-  const onmessage = async () => Promise.reject(new Error())
+  const msg = { fields: {}, content: Buffer.from('hello world'), properties: {} }
+  const err = new Error('failed to fire trigger')
+  const onmessage = async () => Promise.reject(err)
 
   const queue = 'some-queue-name'
   const channel = {
     prefetch: () => {},
-    consume: async (_, cb) => callback = cb,
-    ack: async () => t.fail()
+    checkQueue: async () => {},
+    consume: async (_, cb) => callback = cb
   }
-  await QueueProcessor(channel, queue, onmessage)
-  await t.throwsAsync(callback)
+  const qp = await QueueProcessor(channel, queue, onmessage, logger)
+  qp.on('error', _err => t.is(_err, err))
 
+  await callback(msg)
+
+  return new Promise(resolve => setTimeout(resolve, 1))
+})
+
+test('should throw error if channel.consume fails', async t => {
+  t.plan(1)
+
+  const channel = {
+    prefetch: () => {},
+    checkQueue: async () => {},
+    consume: async () => Promise.reject(new Error('Some error message'))
+  }
+  await t.throwsAsync(() => QueueProcessor(channel, null, null, logger), 'Some error message')
+})
+
+test('should throw error if queue does not exist', async t => {
+  t.plan(1)
+
+  const channel = {
+    prefetch: () => {},
+    checkQueue: async () => Promise.reject(new Error('Some error message'))
+  }
+  await t.throwsAsync(() => QueueProcessor(channel, null, null, logger), 'Some error message')
+})
+
+test('should emit error event if channel.ack fails', async t => {
+  t.plan(1)
+
+  let callback = null
+  const msg = { fields: {}, content: Buffer.from('hello world'), properties: {} }
+  const onmessage = async () => {}
+
+  const queue = 'some-queue-name'
+  const err = new Error('ack failed')
+  const channel = {
+    prefetch: () => {},
+    checkQueue: async () => {},
+    consume: async (_, cb) => callback = cb,
+    ack: async () => Promise.reject(err)
+  }
+
+  const qp = await QueueProcessor(channel, queue, onmessage, logger)
+  qp.on('error', _err => t.is(_err, err))
+
+  await callback(msg)
+
+  return new Promise(resolve => setTimeout(resolve, 1))
 })
